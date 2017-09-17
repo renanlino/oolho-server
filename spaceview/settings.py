efef6c6a3f526385cfcb4db932843ecceadaf490
Renan Yuri Lino <renan.lino@gmail.com>
2017-09-16 22:17:06 -0300

    Configura o Whitenoise

--- spaceview/settings.py
+++ spaceview/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
