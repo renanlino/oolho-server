e25940b301fd5ecced1ce20b69bbf27f0d9c9d42
Renan Yuri Lino <renan.lino@gmail.com>
2017-10-09 02:50:39 +0000

    Rollback para associação sem hyperlinks

--- app/serializers.py
+++ app/serializers.py
@@ -1,17 +1,16 @@
 from rest_framework import serializers
 from app.models import Movement, Sensor
 
-class SensorSerializer(serializers.HyperlinkedModelSerializer):
+class SensorSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    movements = serializers.HyperlinkedRelatedField(many=True,
-        view_name='movement-detail', read_only=True)
+    movements = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Sensor
         fields = ('id', 'display_name', 'created_date',
             'last_seen', 'owner', 'movements')
 
-class MovementSerializer(serializers.HyperlinkedModelSerializer):
+class MovementSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
---
+++ fake_sensor.py
@@ -0,0 +1,42 @@
+import random
+import requests
+from requests.auth import HTTPBasicAuth
+import sys
+from datetime import datetime, timedelta
+
+def main():
+    usersInside = 0
+    baseTime = datetime.now()
+    idSensor = int(input("Digite a ID do sensor: "))
+    user = input("Digite o usuário: ")
+    password = input("Digite a senha: ")
+    duration = int(input("Digite a duração máxima da simulação em minutos: "))
+    movement = {
+        "sensor": idSensor,
+        "direction": None,
+        "received_date": None,
+        "occurrence_date": None
+    }
+    maxTime = timedelta(minutes=duration)
+    end = baseTime + maxTime
+    while baseTime < end:
+        timelapse = timedelta( seconds = random.randint(10, 120) )
+        baseTime += timelapse
+        occurrence_date = baseTime.strftime("%Y-%m-%dT%H:%M:%S-03:00")
+        movement["occurrence_date"] = occurrence_date
+        direction = random.choice([1, -1])
+        if direction == -1 and usersInside > 0:
+            movement["direction"] = "OUT"
+        elif direction == -1 and usersInside == 0:
+            movement["direction"] = "IN"
+            direction = 1
+        else:
+            movement["direction"] = "IN"
+        usersInside += direction
+        print("%s\t %s \t(%d users inside)" %(movement["occurrence_date"],
+                movement["direction"], usersInside))
+        response = requests.post("https://gentle-lake-67733.herokuapp.com/movements/",
+                        auth=HTTPBasicAuth(user, password), data=movement)
+        print("\t> Status Code: %d (%s)" %(response.status_code, response.text))
+
+main()
