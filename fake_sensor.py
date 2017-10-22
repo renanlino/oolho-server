02573521daf45a1453fc4678c2ca76595368dfbd
Renan Yuri Lino <renan.lino@gmail.com>
2017-10-22 14:04:34 -0200

    Modifica operação do sensor virtual e move SQL de visualização.

--- fake_sensor.py
+++ fake_sensor.py
@@ -9,8 +9,7 @@ LOCAL = "http://localhost:8000/api/movements/?format=json"
 
 def main():
     usersInside = 0
-    baseTime = datetime.now()
-    idSensor = int(input("Digite a ID do sensor: "))
+    idSensor = input("Digite a ID do sensor: ")
     duration = int(input("Digite a duração máxima da simulação em horas: "))
     user = input("Digite o usuário (eletricademo): ")
     if user == '':
@@ -20,43 +19,49 @@ def main():
         password = "140897hr"
     server = input("Local (l) ou Produção (P)?: ")
     offset = input("Digite o offset em horas (0): ")
-    if offset != '':
-        offset = timedelta(hours = int(offset))
-        baseTime += offset
+    listSensor = idSensor.split(",")
+    for idSensor in listSensor:
+        idSensor = int(idSensor)
+        baseTime = datetime.now()
+        if offset != '':
+            toffset = timedelta(hours = int(offset))
+            baseTime += toffset
 
-    if server.upper() == "L":
-        URL = LOCAL
-    else:
-        URL = PROD
-    movement = {
-        "sensor": idSensor,
-        "direction": None,
-        "received_date": None,
-        "occurrence_date": None
-    }
-    maxTime = timedelta(hours=duration)
-    end = baseTime + maxTime
-    while baseTime < end:
-        quantity = random.randint(-usersInside, usersInside + 1)
-        if quantity != 0:
-            timelapse = timedelta( minutes = random.randint(15, 45) )
-            baseTime += timelapse
-            occurrence_date = baseTime.strftime("%Y-%m-%dT%H:%M:%SZ")
-            movement["occurrence_date"] = occurrence_date
-            movement["value"] = abs(quantity)
-            direction = abs(quantity)/quantity
-            if direction == -1:
-                movement["direction"] = "OUT"
-            else:
-                movement["direction"] = "IN"
-            usersInside += quantity
-            print("%s\t %s \t(%d users inside)" %(movement["occurrence_date"],
-                    movement["direction"], usersInside))
-            response = requests.post(URL,
-                            auth=HTTPBasicAuth(user, password), data=movement)
-            if response.status_code > 300:
-                print("\t> Status Code: %d (%s)" %(response.status_code, response.text))
-            else:
-                print("\t> Status Code: %d" %(response.status_code))
+        if server.upper() == "L":
+            URL = LOCAL
+        else:
+            URL = PROD
+        movement = {
+            "sensor": idSensor,
+            "direction": None,
+            "received_date": None,
+            "occurrence_date": None
+        }
+        maxTime = timedelta(hours=duration)
+        end = baseTime + maxTime
+        while baseTime < end:
+            quantity = random.randint(-usersInside, usersInside + 1)
+            if abs(quantity) > 50:
+                continue
+            if quantity != 0:
+                timelapse = timedelta( minutes = random.randint(15, 45) )
+                baseTime += timelapse
+                occurrence_date = baseTime.strftime("%Y-%m-%dT%H:%M:%SZ")
+                movement["occurrence_date"] = occurrence_date
+                movement["value"] = abs(quantity)
+                direction = abs(quantity)/quantity
+                if direction == -1:
+                    movement["direction"] = "OUT"
+                else:
+                    movement["direction"] = "IN"
+                usersInside += quantity
+                print("%s\t %s \t(%d users inside)" %(movement["occurrence_date"],
+                        movement["direction"], usersInside))
+                response = requests.post(URL,
+                                auth=HTTPBasicAuth(user, password), data=movement)
+                if response.status_code > 300:
+                    print("\t> Status Code: %d (%s)" %(response.status_code, response.text))
+                else:
+                    print("\t> Status Code: %d" %(response.status_code))
 
 main()
