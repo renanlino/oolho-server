e9dc3e1d686fec488a62eb2ca0ad2f93ad818cf3
Renan Yuri Lino <renan.lino@gmail.com>
2017-11-06 22:28:08 -0200

    Novo nome e domínio

--- fake_sensor.py
+++ fake_sensor.py
@@ -4,7 +4,7 @@ from requests.auth import HTTPBasicAuth
 import sys
 from datetime import datetime, timedelta
 
-PROD = "https://adeusdentinho.herokuapp.com/api/movements/?format=json"
+PROD = "https://oolho.herokuapp.com/api/movements/?format=json"
 LOCAL = "http://localhost:8000/api/movements/?format=json"
 
 def main():
